a22acbcf2b242786c1433fd5d5def2a5982933c2
theac <theac@DESKTOP-LEARA2L>
2024-01-08 19:30:27 +0300

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccountBase<T>
+    {
+        private T accountNumber { get; set; }
+        private double balance { get; set; }
+        private string ownerFullName { get; set; }
+
+        public void InPut()
+        {
+            Console.WriteLine("Введите номер счета:");
+            accountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс счета:");
+            balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца счета:");
+            ownerFullName = Console.ReadLine();
+        }
+
+        public string GetInfo()
+        {
+            return $"Account Number:{accountNumber}\nBalance:{balance}\nOwner Full Name:{ownerFullName}";
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccountBase<int> bankAccount1 = new BankAccountBase<int>();
+            bankAccount1.InPut();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            BankAccountBase<string> bankAccount2 = new BankAccountBase<string>();
+            bankAccount2.InPut();
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
